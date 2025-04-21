a0356aeddf93ae6730f73840b2430190f92c40d7
Verebell <verebell2020@gmail.com>
2025-04-22 03:44:36 +0900

    Update files
    파일명 수정

--- LinearDataStructure/LinearDataStructure/DataStructAlgorithm.cpp
+++ LinearDataStructure/LinearDataStructure/DataStructAlgorithm.cpp
@@ -11,16 +11,5 @@ using namespace std;
 
 int main()
 {
-	vector<string> players = {
-		"µå·¡°ï ¿öÅÍ", "ÃÑÀÌ ¾ø´Â ¼Ò", "Àë¹Î", "ÀçÇõÀÌ"
-	};
-
-	CardGame NewCardGame;
-
-	for (auto player : players)
-	{
-		NewCardGame.AddPlayer(player);
-	}
-
-	NewCardGame.PlayGame();
+	
 }
